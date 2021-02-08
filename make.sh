c933523cee220580387f70e7e920aac81fadfba3
Moises Baltazar <me@moisesb.com>
2021-02-08 06:12:07 -0600

    Move singularity code into subfolder

--- make.sh
+++ make.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/bash
 echo 'Building the "Intermediate" container.'
-sudo singularity build intermediate.sif intermediate.def
+sudo singularity build container/intermediate.sif container/intermediate.def
 
 
 
 echo 'Using the "Intermediate" container to build alpine-compatible singularity.'
-rm -rf ./output
-mkdir ./output
+rm -rf ./container/output
+mkdir ./container/output
 
 singularity exec \
-    --bind ./output:/usr/local/ \
-    --bind ./build-singularity.sh:/tmp/build-singularity.sh \
-    ./intermediate.sif \
+    --bind ./container/output:/usr/local/ \
+    --bind ./container/build-singularity.sh:/tmp/build-singularity.sh \
+    ./container/intermediate.sif \
     /tmp/build-singularity.sh
 
 
 
 echo 'Building the "Final" container.'
-sudo singularity build final.sif final.def
+sudo singularity build xnat-remote.sif container/final.def
